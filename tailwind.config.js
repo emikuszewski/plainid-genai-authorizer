495083bac802826b468f05e1dcbce76758d7b40c
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-08-19 11:49:29 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'plainid-teal': '#00A7B5',
+        'deep-teal': '#002A3A',
+        'misty-teal': '#D1E4E5',
+        'icy-gray': '#EEF1F4',
+        'cloudy-gray': '#BFCED6',
+      },
+      animation: {
+        'bounce-slow': 'bounce 2s infinite',
+        'pulse-slow': 'pulse 3s infinite',
+        'shimmer': 'shimmer 3s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
